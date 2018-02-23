12257fc615f60138863ed36469d7f7635d505f61
nisam <nisam@rexav.in>
2018-02-23 11:58:08 +0530

    addedimage link and content above in programpage

--- src/app/programs/programs.component.ts
+++ src/app/programs/programs.component.ts
@@ -11,9 +11,9 @@ export class ProgramsComponent implements OnInit {
   public programs: any = [];
 
   constructor() {
-      this.programs.push({image:'image',h1:'this  my 1st title', para: 'lorum gymsum fghjkl'});
-      this.programs.push({image:'image',h1:'this is 2nd title', para: 'lorum gymsum fghjkl'});    
-      this.programs.push({image:'image',h1:'this is 3d title', para: 'lorum gymsum fghjkl'});   
+      this.programs.push({image:'/assets/images/img-3-1.jpg',h1:'this  my 1st title', para: 'lorum gymsum fghjkl'});
+      this.programs.push({image:'/assets/images/img-3-1.jpg',h1:'this is 2nd title', para: 'lorum gymsum fghjkl'});    
+      this.programs.push({image:'/assets/images/img-3-1.jpg',h1:'this is 3d title', para: 'lorum gymsum fghjkl'});   
   }
 
   ngOnInit() {
